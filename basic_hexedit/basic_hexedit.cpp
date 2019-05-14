d8d27141982256bfe9191cd48b8e23a9078070ce
alexp205 <politowicz205@gmail.com>
2019-05-13 23:51:13 -0500

    basic start

---
+++ basic_hexedit/basic_hexedit.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    if (2 != argc) {
+        printf("usage: basic_hexedit <filepath>\n");
+        exit(1);
+    }
+
+    // setup
+
+
+    // read in file
+
+
+    // read line
+
+
+    // display as:
+    //  - binary
+
+
+    //  - hex
+
+
+    //  - ascii
+
+
+    // clean up
+
+    return 0;
+}
