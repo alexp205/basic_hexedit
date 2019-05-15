aa145496b84cef7d3ebfeb11fed20887834b1701
alexp205 <politowicz205@gmail.com>
2019-05-14 23:33:39 -0500

    added read functionality and some test cases

--- basic_hexedit/basic_hexedit.cpp
+++ basic_hexedit/basic_hexedit.cpp
@@ -1,35 +1,141 @@
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
+#include <sstream>
+#include <iomanip>
 #include <string>
+#include <stdio.h>
+
+using namespace std;
+
+const int OFFSET_PER_LINE = 10; // number of (hex) characters (not including last space and col space)
+const int HEX_PER_LINE = 48; // number of hex values in form of "xx " (not including col space)
+const int ASCII_PER_LINE = 16; // number of ascii characters
+int lines_per_window = 20;
 
 int main(int argc, char *argv[])
 {
+    // setup
+    string option_str = "-r";
+    string filepath;
+    int mode = 1; // 1 = read, 2 = write
+    int offset = 0;
+
+    // process args
     if (2 != argc) {
-        printf("usage: basic_hexedit <filepath>\n");
+        if (3 != argc) {
+            printf("usage: basic_hexedit [-r/w] <filepath>\n");
+            exit(1);
+        }
+        option_str = argv[1];
+        filepath = argv[2];
+    }
+    else {
+        filepath = argv[1];
+    }
+
+    if (option_str.compare("-w") == 0) {
+        mode = 2;
+    } else if (option_str.compare("-r") == 0) {
+        mode = 1;
+    } else {
+        printf("usage: basic_hexedit [-r/w] <filepath>\n");
         exit(1);
     }
 
-    // setup
+    if (1 == mode) {
+        // read in file
+        ifstream f;
+        f.open(filepath, fstream::binary);
+        if (!f.is_open()) {
+            printf("[ERROR] error opening file\n");
+            exit(1);
+        }
 
+        // read line
+        string hex_str;
+        string ascii_str;
+        int idx = 0;
+        int start_offset = 0;
+        int end_offset = 0;
 
-    // read in file
+        char c;
+        while (f.get(c)) {
+            if (lines_per_window == end_offset - start_offset) {
+                // wait for input
+                char input = '.';
+                while ('j' != input && 'k' != input) {
+                    cin >> input;
+                    if ('j' == input) {
+                        start_offset += ASCII_PER_LINE;
+                    } else if ('k' == input) {
+                        if (0 == start_offset) {
+                            input = '.';
+                        } else {
+                            start_offset -= ASCII_PER_LINE;
+                        }
+                    }
+                }
 
+                // clear output
+                for (int i = 0; i < lines_per_window; i++) {
+                    printf("\n");
+                }
 
-    // read line
+                // reset file offset
+                f.seekg(start_offset);
+                end_offset = start_offset;
+            } else {
+                // continue reading
+                if (isprint(c)) {
+                    string s(1, c);
+                    ascii_str.append(s);
+                }
+                else {
+                    string s(1, '.');
+                    ascii_str.append(s);
+                }
+                stringstream sstream_hex;
+                sstream_hex << setfill('0') << setw(2) << hex << (int)c << " ";
+                hex_str.append(sstream_hex.str());
 
+                if (15 == idx % ASCII_PER_LINE) {
+                    stringstream sstream;
+                    sstream << setfill('0') << setw(OFFSET_PER_LINE) << hex << offset++;
+                    string offset_str = sstream.str();
+                    cout << offset_str << "  " << hex_str << " " << ascii_str << endl;
 
-    // display as:
-    //  - binary
+                    hex_str = "";
+                    ascii_str = "";
+                    end_offset++;
+                }
 
+                idx++;
+            }
+        }
+        if (0 != idx % ASCII_PER_LINE) {
+            stringstream sstream;
+            sstream << setfill('0') << setw(OFFSET_PER_LINE) << hex << offset++;
+            string offset_str = sstream.str();
+            cout << offset_str << "  " << left << setfill(' ') << setw(HEX_PER_LINE) << hex_str << " " << ascii_str << endl;
+        }
 
-    //  - hex
+        // clean up
+        f.close();
+    } else {
+        // read in file
+        fstream f;
+        f.open(filepath, fstream::binary);
+        if (!f.is_open()) {
+            printf("[ERROR] error opening file\n");
+            exit(1);
+        }
 
+        // read line
+        // TODO
 
-    //  - ascii
-
-
-    // clean up
+        // clean up
+        f.close();
+    }
 
     return 0;
 }
